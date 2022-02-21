ce1b80567d31a5ccda0643ba59599fd15fbdd3e1
Mukhtar <mukhtar.mukhamejan@mail.ru>
2022-02-22 00:12:23 +0300

    First commit

---
+++ GameConstants.java
@@ -0,0 +1,8 @@
+public interface GameConstants {
+    public final int DEFAULT_WIDTH = 600;
+    public final int DEFAULT_HEIGHT = 300;
+    public final int DELAY = 8;
+    public final int BASERADIUS=5;
+    public final int LIFETIME=1300;
+    public final int MAXRADIUS=25;
+    public final int STARTQNTBALLS=10;
